c7488dd31740c09b9fa8b9b82f57693e294c4d1a
Vishal Singh Chauhan <70084496+vishaldpw@users.noreply.github.com>
2022-09-22 15:31:38 +0530

    Create main.ps1

---
+++ main.ps1
@@ -0,0 +1,13 @@
+$SnowInstance = 'dev114585.service-now.com'
+$SnowUserName = 'admin'
+$SnowPass = 'L2Ia8^IiMd!e'
+$ShortDescription = 'reboot'
+
+$SnowPass1 = $SnowPass | ConvertTo-SecureString -AsPlainText -Force
+$snowCreds = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $SnowUserName,$SnowPass1
+$method = "GET"
+$uri = "https://$SnowInstance/api/now/table/change_request?sysparm_query=short_descriptionLIKE$ShortDescription&sysparm_display_value=true"
+
+$response = Invoke-WebRequest -ContentType application/json -Method $method -Uri $uri -Credential $snowCreds|ConvertFrom-Json
+$ChangeNumber = $response.result.state
+return $ChangeNumber
